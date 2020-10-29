3f78b3d2b8cbedce2c62badb07aa8589064d276d
SeanDoughty <sean.doughty@outlook.com>
2020-10-29 16:43:35 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from diskspring import *
+import csv
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    Data = pd.read_csv("spring_input.csv")
+    Table = Data.values.tolist()
+    Columns = Data.columns
+
+    for i in range(len(Table)):
+        spring = DiscSpring(Table[i][0:7])
+
+        max_s = 0.75 * spring.h0
+
+        Table[i][7] = max_s
+        Table[i][8] = spring.find_force(max_s)
+        Table[i][9:14] = spring.find_stress(max_s)
+
+        spring.plot_force(0,max_s+1)
+
+    Output = pd.DataFrame(Table, columns=Columns)
+    Output.to_csv("spring_input.csv", index=False)
+    
+
+   
+
+
+
+
+
+
+
+ 
