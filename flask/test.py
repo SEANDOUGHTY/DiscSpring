da555f535510691cb34545461c883690b90b569f
SeanDoughty <sean.doughty@outlook.com>
2020-11-13 17:14:26 -0500

    fix: adding test cases

--- flask/test.py
+++ flask/test.py
@@ -1,6 +1,7 @@
 import run
 import unittest
 from app.discspring import *
+from flask import request
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -10,10 +11,14 @@ class TestApp(unittest.TestCase):
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-
+    
     def test_Force(self):
         spring = DiscSpring([200,180,8,3,1,1], "Ti-6Al-4V", 108500, 0.34)
         self.assertAlmostEqual(spring.find_force(2), 8526.04, 1)
+
+    def test_Stress(self):
+        spring = DiscSpring([200,180,8,3,1,1], "Ti-6Al-4V", 108500, 0.34)
+        self.assertAlmostEqual(spring.find_stress(2)[1], -944.45, 1)
 
 if __name__ == '__main__':
     unittest.main()
