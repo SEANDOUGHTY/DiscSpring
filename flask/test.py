37516b8abab4d45f79abf3946f03e968179bfa90
SeanDoughty <sean.doughty@outlook.com>
2020-11-13 14:40:29 -0500

    adding test cases

---
+++ flask/test.py
@@ -0,0 +1,14 @@
+import run
+import unittest
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = run.app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
